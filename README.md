929b9d0a948cc53785cc35083f1756297758fd97
Shanmugamrskfamily <shanmugamrskfamily@email.com>
2023-09-21 12:16:44 +0530

    Readme update with proper comments

---
+++ README.md
@@ -0,0 +1,82 @@
+# Nodejs- Filesystem
+
+A Node.js application that provides a simple file management system with RESTful API endpoints. Users can create timestamped text files and retrieve a list of existing text files stored in the "Output" directory. The API is documented for easy integration and usage.
+
+## Table of Contents
+
+- [Getting Started](#getting-started)
+  - [Prerequisites](#prerequisites)
+  - [Installation](#installation)
+- [Usage](#usage)
+- [API Documentation](#api-documentation)
+- [Contributing](#contributing)
+- [License](#license)
+
+## Getting Started
+
+### Prerequisites
+
+- Node.js and npm installed on your machine.
+
+### Installation
+
+1. Clone the repository:
+
+   ```shell
+   git clone https://github.com/Shanmugamrskfamily/nodejs-filesystem.git
+   ```
+
+2. Navigate to the project directory:
+
+```shell
+cd nodejs-filesystem
+```
+
+3. Install the dependencies:
+
+```shell
+npm install
+```
+
+## Usage
+
+POST: Create Text File
+http://localhost:3000/createTextFile
+
+1. Send the Request:
+
+- After configuring the request, click the "Send" button to send the POST request to the specified URL.
+
+2. Response:
+
+- You will receive a response from the server, which may indicate whether the file creation was successful or not. Be sure to handle the response accordingly in your application.
+
+GET: List Text Files
+http://localhost:3000/getTextFiles
+
+1. Send the Request:
+
+- After configuring the request, click the "Send" button to send the GET request to the specified URL.
+
+2. Response:
+
+- You will receive a response from the server containing the list of text files created using the previous POST requests. The server's response the file names of text files.
+- You can view the list of text files in the Postman response panel.
+
+## API Documentation
+
+You can find the API documentation for this project https://documenter.getpostman.com/view/28858691/2s9YC7SBXQ
+
+## Contributing
+
+If you'd like to contribute to this project, please follow these guidelines:
+Fork the project.
+Create a new branch for your feature or bug fix.
+Make your changes and commit them with descriptive messages.
+Push your changes to your fork.
+Submit a pull request to the main branch of the original repository.
+Please make sure to update tests as appropriate.
+
+## License
+
+This project is licensed under the [License Name] License - see the LICENSE file for details.
--- server.js
+++ server.js
@@ -2,53 +2,65 @@ const express = require('express');
 const app = express();
 const fs = require('fs');
 const path = require('path');
-const ouputFolder="./Output";
-if (!fs.existsSync(ouputFolder))
-{
-    fs.mkdirSync(ouputFolder)
+
+// Define the output directory for text files
+const outputFolder = "./Output";
+
+// Ensure the output directory exists; create it if it doesn't
+if (!fs.existsSync(outputFolder)) {
+  fs.mkdirSync(outputFolder);
 }
-const PORT =3000;
+
+const PORT = 3000;
 
 app.use(express.json());
 
-// Set up your routes here
+// Define routes
 
 app.listen(PORT, () => {
-  console.log(`Server is running : http://localhost:${PORT}`);
+  console.log(`Server is running: http://localhost:${PORT}`);
 });
 
+/**
+ * Create a timestamped text file.
+ */
 app.post("/createTextFile", (req, res) => {
-    const currentTime = new Date();
-    const year = currentTime.getFullYear().toString();
-    const month = (currentTime.getMonth() + 1).toString();
-    const date = currentTime.getDate().toString();
-    const hrs = currentTime.getHours().toString();
-    const mins = currentTime.getMinutes().toString();
-    const secs = currentTime.getSeconds().toString();
-  
-    const dateTimeForFileName = `${year}-${month}-${date}-${hrs}-${mins}-${secs}.txt`;
-  
-    const filePath = path.join(ouputFolder, dateTimeForFileName);
-  
-    fs.writeFile(filePath, currentTime.toISOString(), (err) => {
-      if (err) {
-        res.status(500).send(`Error creating file: ${err}`);
-        return;
-      }
-  
-      res.send(`File created successfully at: ${filePath}`);
-    });
+  const currentTime = new Date();
+  const year = currentTime.getFullYear().toString();
+  const month = (currentTime.getMonth() + 1).toString().padStart(2, '0');
+  const date = currentTime.getDate().toString().padStart(2, '0');
+  const hrs = currentTime.getHours().toString().padStart(2, '0');
+  const mins = currentTime.getMinutes().toString().padStart(2, '0');
+  const secs = currentTime.getSeconds().toString().padStart(2, '0');
+
+  const dateTimeForFileName = `${year}-${month}-${date}-${hrs}-${mins}-${secs}.txt`;
+
+  const filePath = path.join(outputFolder, dateTimeForFileName);
+
+  fs.writeFile(filePath, currentTime.toISOString(), (err) => {
+    if (err) {
+      res.status(500).send(`Error creating file: ${err}`);
+      return;
+    }
+
+    res.send(`File created successfully at: ${filePath}`);
   });
+});
+
+/**
+ * Get a list of text files in the output directory.
+ */
+app.get('/getTextFiles', (req, res) => {
+  fs.readdir(outputFolder, (err, files) => {
+    if (err) {
+      res.status(500).send(`Error occurred while reading files: ${err}`);
+      return;
+    }
     
-  app.get('/getTextFiles',(req,res)=>{
-    fs.readdir(ouputFolder,(err,files)=>{
-        if(err){
-            res.status(500).send(`Error Occured on Reading Files: ${err}`);
-            return
-        }
-        console.log("List of files: \n", files);
-        const textFiles = files.filter((file) => path.extname(file) === ".txt");
+    console.log("List of files:\n", files);
     
-        res.json(textFiles);
-    })
+    const textFiles = files.filter((file) => path.extname(file) === ".txt");
+    
+    res.json(textFiles);
+  });
 });
